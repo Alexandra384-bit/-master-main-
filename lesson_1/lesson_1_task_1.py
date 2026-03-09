26beb7f293ecb664cce2e59a19284e640f95351b
Alexandra384-bit <kasandra1730@yandex.ru>
2026-03-09 14:49:29 +0500

    Удаление

--- lesson_1/lesson_1_task_1.py
+++
@@ -1,3 +0,0 @@
-#coding: utf-8
-my_name = "Александра"
-print(my_name)
--- lesson_2/lesson_2_task_6.py
+++
@@ -1,5 +0,0 @@
-lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-
-for value in lst:
-    if value < 30 and value % 3 == 0:
-        print(value)
